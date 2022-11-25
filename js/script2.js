533b3f7bf3ae344209000a0f37be6994221dcd1d
FlorianKomnino <florian.komnino@outlook.it>
2022-11-25 16:05:00 +0100

    completed snack 2

--- js/script2.js
+++ js/script2.js
@@ -1,2 +1,9 @@
 // Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
 
+const requestedNumber = parseInt(prompt('Inserisci un numero'));
+
+if (requestedNumber % 2 == 0) {
+    console.log(requestedNumber);
+} else {
+    console.log(requestedNumber + 1);
+}
