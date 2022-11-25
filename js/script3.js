6a51cff728f8a965bd37a848da3627749d59fe45
FlorianKomnino <florian.komnino@outlook.it>
2022-11-25 17:09:08 +0100

    completed snack 3 third part

--- js/script3.js
+++ js/script3.js
@@ -11,11 +11,14 @@ let numbersSum = 0;
 
 let averageOperator = 0;
 
-
+let maxNumber = 0;
 
 for (let i = 0 ; i < 10 ; i++) {
     numbersSum = numbersSum + myExampleArray[i];
     averageOperator += 1;
+    if (maxNumber < myExampleArray[i]) {
+        maxNumber = myExampleArray[i];
+    }
 }
 
 let numericalAverage = numbersSum / averageOperator;
@@ -23,5 +26,7 @@ let numericalAverage = numbersSum / averageOperator;
 console.log(numbersSum);
 
 console.log(numericalAverage);
+
+console.log(maxNumber);
 
 // Calcola la somma e la media dei primi 10 numeri di un array.
