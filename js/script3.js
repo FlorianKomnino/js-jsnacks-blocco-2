904b45387ee50592f90ff8912e7774c8eb69fd5e
FlorianKomnino <florian.komnino@outlook.it>
2022-11-25 16:49:20 +0100

    completed snack 3 second part

--- js/script3.js
+++ js/script3.js
@@ -9,8 +9,19 @@ console.log(myExampleArray);
 
 let numbersSum = 0;
 
+let averageOperator = 0;
+
+
+
 for (let i = 0 ; i < 10 ; i++) {
     numbersSum = numbersSum + myExampleArray[i];
+    averageOperator += 1;
 }
 
+let numericalAverage = numbersSum / averageOperator;
+
 console.log(numbersSum);
+
+console.log(numericalAverage);
+
+// Calcola la somma e la media dei primi 10 numeri di un array.
