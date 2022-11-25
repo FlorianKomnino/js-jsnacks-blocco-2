96025685081b4bd4803acc75eb3ceeb02690f8ea
FlorianKomnino <florian.komnino@outlook.it>
2022-11-25 15:46:11 +0100

    chabged array name

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@ console.log('Hello World!');
 // Chiedi per 6 volte all'utente di inserire un numero,
 // se è dispari inseriscilo nell'array.
 
-let emptyArray = [];
+let exampleArray = [];
 
 let requestedNumber;
 
 for (let i = 0 ; i < 6 ; i++) {
     requestedNumber = parseInt(prompt('Inserisci un numero'));
     if (requestedNumber % 2 != 0) {
-        emptyArray.push(requestedNumber);
+        exampleArray.push(requestedNumber);
     }
 }
 
-console.log(emptyArray);
+console.log(exampleArray);
