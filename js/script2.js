f3e3569c203ea5a973f9fe74ef0d7c62c0be4894
FlorianKomnino <florian.komnino@outlook.it>
2022-11-25 16:14:55 +0100

    another version of snack 2 solution

--- js/script2.js
+++ js/script2.js
@@ -1,9 +1,8 @@
 // Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
 
-const requestedNumber = parseInt(prompt('Inserisci un numero'));
+let requestedNumber = parseInt(prompt('Inserisci un numero'));
 
-if (requestedNumber % 2 == 0) {
-    console.log(requestedNumber);
-} else {
-    console.log(requestedNumber + 1);
+if (requestedNumber % 2 != 0) {
+    requestedNumber = requestedNumber + 1;
 }
+console.log (requestedNumber)
